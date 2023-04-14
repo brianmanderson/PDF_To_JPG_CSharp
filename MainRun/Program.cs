ea1a72cabf187a0eb61b88bdf9921c63713b83a7
brianmanderson <markba122@gmail.com>
2023-04-13 17:08:49 -0700

    update

--- MainRun/Program.cs
+++ MainRun/Program.cs
@@ -1,23 +1,25 @@
+﻿using PDF_To_JPEGClass;
 using System;
-using System.Threading;
+using System.Collections.Generic;
 using System.IO;
-using PDF_To_JPEGClass;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-
-
-namespace PDF_to_JPG
+namespace MainRun
 {
-    class Program
+    internal class Program
     {
         // static string file_path = @"C:\Users\markb\OneDrive - University of California, San Diego Health\Documents";
-        static string[] file_paths = { @"\\ro-ariaimg-v\va_data$\HDR\Bravos\Documents", @"\\ro-ariaimg-v\va_data$\HDR\Report File Export" };
+        static string[] file_paths = { @"C:\Users\markb\Downloads\Test", @"\\ro-ariaimg-v\va_data$\HDR\Bravos\Documents", @"\\ro-ariaimg-v\va_data$\HDR\Report File Export" };
         static void Main(string[] args)
         {
             Console.WriteLine("Running...");
             PdfToJpeg pdf_writer = new PdfToJpeg();
             while (true)
             {
-                Thread.Sleep(1000);
+                Thread.Sleep(3000);
                 foreach (string file_path in file_paths)
                 {
                     if (!Directory.Exists(file_path))
